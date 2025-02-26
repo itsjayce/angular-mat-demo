366e137e8bfa43157b9bce8684a368e58e8e0063
Jas <tinadumpschool@gmail.com>
2025-02-26 11:01:40 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button'
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    RegisterComponent, 
+    ReactiveFormsModule, 
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSliderModule, 
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
